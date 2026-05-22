09d0d24ed61b06dcce7dbcce215fc61d63b0429f
ömer faruk inel <omer.inel@ciellos.com>
2026-05-22 22:51:56 +0300

    v1.1.0

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ return {
     name        = "simpleui_ext",
     fullname    = "SimpleUI Extra Modules",
     description = [[Extra modules for SimpleUI.]],
-    version     = "1.0.1",
+    version     = "1.1.0",
 }
