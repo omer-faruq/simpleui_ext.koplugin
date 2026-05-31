180da2085069f239a483a1ec421964cdafee419c
ömer faruk inel <omer.inel@ciellos.com>
2026-05-31 16:01:23 +0300

    v1.8.0

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ return {
     name        = "simpleui_ext",
     fullname    = "SimpleUI Extra Modules",
     description = [[Extra modules for SimpleUI with enable/disable toggles.]],
-    version     = "1.7.1",
+    version     = "1.8.0",
 }
